d30f4f727c293fa53072bdd1496557f13d40b3ab
daniherr <daniherr@bancolombia.com.co>
2022-06-08 11:11:51 -0500

    dsa

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
 window.onload = () => {
     document.querySelectorAll(".open-modal-button").forEach(element => {
-        console.log("pimche")
-        element.addEventListener("click", e => {
-            openModal(e);
-            console.log('muere boton');
-        });
-    });
-    document.querySelectorAll(".pepe").forEach(element => {
-        console.log("pimche")
-        element.addEventListener("click", e => {
-            openModal(e);
-            console.log('muere pepe');
-        });
-    });
-    document.getElementById('pe').addEventListener("click", e => {
-        openModal(e);
-        console.log('muere');
+        console.log(element);
+        element.addEventListener("click", e => openModal(e));
     });
     document.body.addEventListener("click", e => closeModal(e));
     document.body.addEventListener("keyup", e => listenForEsc(e));
-    
 };
 /**Escucha por la clave esc para cerrar el modal */
 function listenForEsc(e) {
@@ -27,18 +12,16 @@ function listenForEsc(e) {
         closeModal(e)
     }
 }
-
 /** Esta funcion se llama cuando la persona hace click en cualquier porjecto del carousel */
 function openModal(e) {
     document.querySelector(".modal-container").style.display = "flex";
     document.getElementById('modal-header').focus();
 }
-
 /** Esta funcion se llama para cerrar el modal */
 function closeModal(e) {
     // si el click occurio dentro del las imagenes del carousel o dentro del modal, no se cierra el modal
     if (
-        e.target.className.includes("project") ||
+        e.target.className.includes("open-modal-button") ||
         e.target.className === "modal"
     ) {
         return;
